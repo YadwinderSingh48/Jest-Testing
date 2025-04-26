92101f7fc8196a54a237a7c6402e49e6d0cc753f
Yadwinder <yadwindermehra8691@gmail.com>
2025-04-27 00:13:07 +0530

    First Test Suite Created

---
+++ index.test.js
@@ -0,0 +1,10 @@
+const multiply = require('./index');
+
+// Create the first Test Suite
+
+// TEST SUITE is a single file where we can define the multiple test cases
+// test is a global keyword when we install jest as dev dependency
+
+test('2 and 5 multiply should be 10', () => { 
+    expect(multiply(2,5)).toBe(10); // expect will state what we expecting and toBe states the exact equality. tobBe is a matcher is test cases and we habe many kind of matchers
+ })
